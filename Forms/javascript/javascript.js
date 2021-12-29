a299f9fb69c0b139c84df35124902c3d52fe02df
sanshar5 <sansahr5leo@gmail.com>
2021-12-29 21:21:19 +0545

    learning java script

---
+++ Forms/javascript/javascript.js
@@ -0,0 +1,14 @@
+let difference = 45 - 15;
+console.log(difference)
+
+let divide = 66 / 11;
+console.log(divide)
+
+let increment = 99;
+increment++;
+console.log(increment)
+
+let decimal = 0.9 * 0.1;
+console.log(decimal)
+
+
