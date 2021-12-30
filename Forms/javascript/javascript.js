d316e9368dc96d85f28f9722e8a7d19253d2e6cf
sanshar5 <sansahr5leo@gmail.com>
2021-12-30 20:46:19 +0545

    learning javascript

--- Forms/javascript/javascript.js
+++ Forms/javascript/javascript.js
@@ -12,3 +12,37 @@ let decimal = 0.9 * 0.1;
 console.log(decimal)
 
 
+let c = 19;
+let d = 11;
+let e = 15;
+
+c += e;
+d = 17 + c;
+console.log(c)
+
+var mystring = "FirstLine\n\tSecondLine\nThirdLine\n\t FourthLine";
+console.log(mystring)
+
+let myname = "Sanshar Sharma";
+let mystr = "Hello my name is " + myname + " and I live in Pokhara.";
+firstLetterofFirstName = myname [8];
+console.log(firstLetterofFirstName)
+console.log(mystr)
+
+let adj = "good boy.";
+let my = "Sanshar Sharma is a ";
+my += adj;
+let length = my.length;
+let lastname = my[my.length-3]
+console.log(lastname)
+console.log(length)
+console.log(my)
+
+
+
+var myword = "Sello Sanshar";
+myword = "Hello Sanshar";
+console.log(myword)
+
+
+
---
+++ Forms/javascript/script.js
@@ -0,0 +1,74 @@
+const numbers = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+};
+
+const operators = {
+    'plus': '+',
+    'minus': '-',
+    'multiply': '*',
+    'divide': '/',
+};
+
+let stack = [];
+let lastInput = '';
+let lastOperator = '';
+let plusMinusIndicator = '+';
+
+function keyClick(id) {
+    const inputRef = document.getElementById('display-result');
+    if (id === 'clear') {
+        inputRef.value = '';
+        plusMinusIndicator = '';
+        document.getElementById('plus-minus-indicator').innerHTML = '';
+        stack = [];
+        return;
+    }
+
+    if (id in numbers) {
+        num = numbers[id];
+        if (lastInput === 'number') {
+            inputRef.value += numbers[id];
+        } else {
+            inputRef.value = numbers[id];
+        }
+        lastInput = 'number';
+    }
+
+    if (id in operators) {
+        if (lastInput === 'operator') {
+            stack[stack.length - 1] = operators[id];
+        } else {
+            stack.push(inputRef.value);
+            stack.push(operators[id]);
+        }
+        lastInput = 'operator';
+    }
+
+    if (id === 'equals') {
+        stack.push(inputRef.value);
+        let expression = stack.join('');
+        const result = eval(expression);
+        inputRef.value = eval(expression);
+        stack = [];
+    }
+
+    if (id === 'plus-minus') {
+        plusMinusIndicator = plusMinusIndicator === '' ? '-' : '';
+        document.getElementById('plus-minus-indicator').innerHTML = plusMinusIndicator;
+    }
+}
+
+function keyPress(e) {
+    if(e.keyCode === 13) {
+        keyClick('equals');
+    }
+}
