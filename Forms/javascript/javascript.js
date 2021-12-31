2603512a55a5890c6120e29e7980298253144ce0
sanshar5 <sansahr5leo@gmail.com>
2021-12-31 17:46:39 +0545

    learning Javascript

--- Forms/javascript/javascript.js
+++ Forms/javascript/javascript.js
@@ -23,9 +23,9 @@ console.log(c)
 var mystring = "FirstLine\n\tSecondLine\nThirdLine\n\t FourthLine";
 console.log(mystring)
 
-let myname = "Sanshar Sharma";
-let mystr = "Hello my name is " + myname + " and I live in Pokhara.";
-firstLetterofFirstName = myname [8];
+let me = "Bishwas Sharma";
+let mystr = "Hello my name is " + me + " and I live in Pokhara.";
+firstLetterofFirstName = me [0];
 console.log(firstLetterofFirstName)
 console.log(mystr)
 
@@ -44,5 +44,91 @@ var myword = "Sello Sanshar";
 myword = "Hello Sanshar";
 console.log(myword)
 
+let section = ["Bishnu","Sanshar","Parbati"];
+section [2] = "Brinda";
+console.log(section)
+
+let myarrey = [["Sanshar","Bishnu","Brinda"],["Bishwas","Mom","Sister"],[1,2,3]];
+let mydata = myarrey[1][1];
+console.log(mydata);
 
 
+let ourarray = ["Aashish","Sanshar","Bishnu","Brinda"];
+ourarray.push("Bishwas","Mamata","Parbati");
+let removedFromourarray = ourarray.pop();
+let shiftFromOurarray = ourarray.shift();
+let unshiftFromourarray = ourarray.unshift("Tulsi Ram Sharma")
+console.log(ourarray);
+
+
+
+function firstcode(){
+    console.log("Hello World");
+}
+firstcode();
+
+function secondcode(){
+    console.log("Hi World");
+}
+secondcode();
+
+function addition(a,b){
+    console.log(a + b);
+}
+addition(9,11);
+
+function multiply(a,b){
+    console.log(a * b);
+}
+multiply(15, 25);
+
+function localscope() {
+    var myvar = 5;
+    console.log(myvar);
+}
+localscope();
+/*
+console.log(myvar); //doesn't work outside the function
+*/
+let minename = "Sanshar Sharma"
+function itsme() {
+   // return my name;
+   let myname = "Ravanaasura"
+    return myname;
+}
+console.log(itsme());
+console.log(minename)
+
+function minus(num) {
+    return num - 9;
+}
+console.log(minus(20));
+
+function times(num){
+    return num * 15;
+}
+console.log(times(5));
+
+/*
+let sum = "9";
+function add(){
+    sum = sum + 6;
+}
+console.log(add)
+
+
+var number = 15;
+function change(number){
+    return (number + 5) / 4;
+}
+console.log(change());
+*/
+
+function nextinline(arr, item){
+    return item;
+}
+var testarr = [1,2,3,4,5]
+
+console.log("Before" + JSON.stringify(testarr));
+console.log(nextinline(testarr, 6));
+console.log("After" + JSON.stringify(testarr));
